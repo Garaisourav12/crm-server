f322f210467986e8ec0ac59f5d3a1bb9c4936cab
Garaisourav12 <garaisourav12@gmail.com>
2025-11-01 01:59:18 +0530

    Global error handler setup

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,35 @@
-import express from 'express';
+import dotenv from 'dotenv';
+import express, { Request, Response } from 'express';
+import {
+  globalErrorHandler,
+  routeNotFoundErrorHandler,
+} from './middlewares/errorHandler';
+
+dotenv.config();
 
 const app = express();
+const port = process.env.PORT || 8000;
 
-app.use(express.static('dist'));
-app.listen(3000, () => {
-  console.log('🚀 Server started on http://localhost:3000');
+app.use(express.json());
+
+// Welcome message
+app.get('/', (req: Request, res: Response) => {
+  res.status(200).json({ message: 'Welcome to the CRM Service' });
+});
+
+// Global error handler (must be last)
+app.use(globalErrorHandler);
+
+// Route not found error handler
+app.use(routeNotFoundErrorHandler);
+
+app.listen(port, () => {
+  console.log(`🚀 Server running on port ${port}`);
+  console.log(`http://localhost:${port}`);
+  // Log on production only
   if (process.env.NODE_ENV === 'production') {
     process.stdout.write(
-      '⚠️ Server started on http://localhost:3000 [Development Build]\n',
+      `⚠️ Server started on http://localhost:${port} [Production Build]`,
     );
   }
 });
